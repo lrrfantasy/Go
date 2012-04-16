4ebd7cf317051d07bd877462fe5735ec612689fe
lrrfantasy <lrr198737@gmail.com>
2012-04-16 23:58:36 +0800

    Read shortcut data from file

--- go.sh
+++ go.sh
@@ -2,6 +2,16 @@
 # Add this to .bashrc
 
 function go(){
+  filename=~/code/go/.config
+  cat $filename | while read line
+  do
+    tmpIFS=$IFS
+    IFS=":"
+    array=($line)
+    echo ${array[0]} ${array[1]}
+    IFS=$tmpIFS
+  done
+
   if [ $1 = 'devtrac' ]; then
     path=~/code/devTrac
   elif [ $1 = 'homepage' ]; then
